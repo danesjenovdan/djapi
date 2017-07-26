8891ef654a24825f625348dee010c317c3cd215b
Muki <filipdobranic@gmail.com>
2017-07-26 11:20:42 +0000

    fix bugs

--- djnd_utils/settings.py
+++ djnd_utils/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'counter',
+    'djnd_landing',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,6 +83,12 @@ DATABASES = {
     }
 }
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join('/home/muki/djstatic/djnd_utils')
+STATIC_URL = 'http://djstatic.knedl.si/djnd_utils/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -100,7 +107,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
