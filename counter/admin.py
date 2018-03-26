8560cfe775383eaa898f960048aebee888fed8ec
Muki <filipdobranic@gmail.com>
2018-03-26 11:37:03 +0000

    add petition filter on Mail address

--- counter/admin.py
+++ counter/admin.py
@@ -1,4 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 from .models import *
 # Register your models here.
+
+class MailAdmin(admin.ModelAdmin):
+    list_filter = ('type_of',)
+
 admin.site.register(Vote)
+admin.site.register(MailAddress, MailAdmin)
