fb792040475fed3dcf7fe1d1112234cec843ee35
Muki <filipdobranic@gmail.com>
2018-10-09 20:27:59 +0000

    revert locker to python2

--- locker/urls.py
+++ locker/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 
 from locker.views import openLock
 
 urlpatterns = [
-    path('unlock/', openLock, name='openLock'),
+    url(r'^unlock/$', openLock, name='openLock'),
 ]
--- locker/views.py
+++ locker/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render, HttpResponse
 
 from django.utils import timezone
