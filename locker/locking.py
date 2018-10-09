93267b7cb0bdff639d07ac2a144d5f882239a5b5
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-10-09 22:09:32 +0200

    unlock for 2 hours instead of 20 seconds

--- locker/locking.py
+++ locker/locking.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 def checkIfExpired(lock):
   if lock.opened:
-    if (lock.activated_at + timedelta(seconds=20)) > timezone.localtime():
+    if (lock.activated_at + timedelta(hours=2)) > timezone.localtime():
       return False
     else:
       lock.expired = True
